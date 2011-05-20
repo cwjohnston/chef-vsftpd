2053204c07ee560afb6469fbbc3e0222e2710970
Cameron Johnston <cameron@needle.com>
2011-05-19 20:45:40 -0600

    adding attributes and template logic for toggling or setting a much larger number of configuration parameters

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,9 @@
 #
 # Copyright 2010 Robert J. Berger
 #
+# Author: Robert J. Berger
+# Author: Cameron W. Johnston
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -16,13 +19,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-default[:vsftpd][:chroot_local_user] = "YES"
-default[:vsftpd][:chroot_users] = Array.new 
-default[:vsftpd][:use_ssl] = true
+
+# Runtime options
+default[:vsftpd][:use_localtime] = false
+default[:vsftpd][:dirmessage_enable] = true
+default[:vsftpd][:nopriv_user] = "nobody"
+default[:vsftpd][:pam_service_name] = "vsftpd"
+default[:vsftpd][:listen_ipv6_exclusively] = false
+# Login options
+default[:vsftpd][:anonymous_enable] = false
+default[:vsftpd][:local_enable] = false
+default[:vsftpd][:write_enable] = false
+# Anonymous user options
+default[:vsftpd][:anon_upload_enable] = false
+default[:vsftpd][:anon_mkdir_write_enable] = false
+default[:vsftpd][:chown_uploads] = false
+default[:vsftpd][:chown_username] = "ftp"
+# umask options
+
+default[:vsftpd][:chroot_local_user] = true
+default[:vsftpd][:chroot_list_enabled] = true
+default[:vsftpd][:chroot_users] = Array.new
+default[:vsftpd][:chroot_list_file] = "/etc/vsftpd.chroot_list"
+# SSL options
+default[:vsftpd][:ssl_enable] = true
 default[:vsftpd][:use_ssl_certs_from_cookbook] = true
+default[:vsftpd][:require_ssl_reuse] = false
 default[:vsftpd][:ssl_cert_path] = "/etc/ssl/certs"
 default[:vsftpd][:ssl_private_key_path] = "/etc/ssl/private"
 default[:vsftpd][:ssl_certs_basename] = "ftp.example.com"
+# Logging options
 default[:vsftpd][:xferlog_enable] = true
+default[:vsftpd][:xferlog_file] = "/var/log/vsftpd.log"
+default[:vsftpd][:xferlog_std_format] = false
+# Session and security options
+default[:vsftpd][:connect_from_port_20] = true
+default[:vsftpd][:async_abor_enable] = true
+default[:vsftpd][:idle_session_timeout] = 600
+default[:vsftpd][:data_connection_timeout] = 120
+# Virtual user options
 default[:vsftpd][:virtual_users_enable] = false
-default[:vsftpd][:virtual_username] = "ftp"
+default[:vsftpd][:guest_username] = "ftp"
+default[:vsftpd][:guest_enable] = true
+default[:vsftpd][:user_config_dir] = "/etc/vsftpd.userconf"
