5e24b46314ed410b67bf876751d94a3b24d0c898
Gerhard Lazu <gerhard@lazu.co.uk>
2011-09-28 01:07:18 +0100

    Taking ownership of this cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "Robert J. Berger"
-maintainer_email "rberger+maintainer@ibd.com"
+maintainer       "Gerhard Lazu"
+maintainer_email "gerhard@lazu.co.uk"
 license          "Apache License, Version 2.0"
 description      "Installs/Configures vsftpd for Secure SSL SFTP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.1.0"
