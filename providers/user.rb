39c35a7e2192194824c72bb9de003923cb17628d
Gerhard Lazu <gerhard@lazu.co.uk>
2012-01-27 09:31:54 +0000

    Using the "preferred" attribute syntax
    User provider clean-up
    WTF was that local_root path hard-coded?

--- providers/user.rb
+++ providers/user.rb
@@ -1,16 +1,16 @@
 action :add do
   bash "Adding #{new_resource.username} to vsftp" do
     code %{
-      htpasswd -b #{node.vsftpd.user_passwd_file} #{new_resource.username} #{new_resource.password}
+      htpasswd -b #{node[:vsftpd][:user_passwd_file]} #{new_resource.username} #{new_resource.password}
     }
     notifies :restart, resources(:service => "vsftpd"), :delayed
   end
 
-  file "/#{node.vsftpd.user_config_dir}/#{new_resource.username}" do
+  file "#{node[:vsftpd][:user_config_dir]}/#{new_resource.username}" do
     owner "root"
     group "root"
     mode 0644
-    content "local_root=/home/gerhard/edi.lazu.ro"
+    content "local_root=#{new_resource.path}"
     notifies :restart, resources(:service => "vsftpd"), :delayed
   end
 end
@@ -18,8 +18,13 @@ end
 action :remove do
   bash "Removing #{new_resource.username} from vsftpd authentication" do
     code %{
-      sed -i '/#{new_resource.username}.*/ d' #{node.vsftpd.user_passwd_file}
+      sed -i '/#{new_resource.username}.*/ d' #{node[:vsftpd][:user_passwd_file]}
     }
+    notifies :restart, resources(:service => "vsftpd"), :delayed
+  end
+
+  file "#{node[:vsftpd][:user_config_dir]}/#{new_resource.username}" do
+    action :delete
     notifies :restart, resources(:service => "vsftpd"), :delayed
   end
 end
@@ -29,7 +34,7 @@ def load_current_resource
     supports :status => true, :stop => true, :start => true, :restart => true
   end
 
-  file node.vsftpd.user_passwd_file do
+  file node[:vsftpd][:user_passwd_file] do
     owner "root"
     group "root"
     mode 0600
--- resources/user.rb
+++ resources/user.rb
@@ -1,7 +1,8 @@
 actions :add, :remove
 
-attribute :username,    :kind_of => String, :name_attribute => true
-attribute :password,    :kind_of => String, :required => true
+attribute :username,  :kind_of => String,  :name_attribute => true
+attribute :password,  :kind_of => String,  :required => true
+attribute :root,      :kind_of => String,  :required => true
 
 def initialize(*args)
   super
