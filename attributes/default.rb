c00a4af5b1abd7dac265374a9fe66f8fa4525f67
Guilhem Lettron <guilhem@lettron.fr>
2012-09-13 16:57:13 +0200

    change behaviour of "local_root" to be more generic

--- attributes/default.rb
+++ attributes/default.rb
@@ -312,4 +312,4 @@ default[:vsftpd][:user_sub_token] = "$USER"
 #
 # This option represents a directory which vsftpd will try to change into after
 # a local (i.e. non-anonymous) login. Failure is silently ignored.
-default[:vsftpd][:local_root] = "/home/$USER"
+default[:vsftpd][:local_root] = nil
