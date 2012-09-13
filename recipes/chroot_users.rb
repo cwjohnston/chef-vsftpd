3f9e0342f05701df5766ae865a1d322c66d06097
Guilhem Lettron <guilhem@lettron.fr>
2012-09-13 14:51:07 +0200

    Template for chroot_users exist. We cna use it

--- recipes/chroot_users.rb
+++ recipes/chroot_users.rb
@@ -1,6 +1,7 @@
-file "/etc/vsftpd/chroot_list.conf" do
+template "/etc/vsftpd/chroot_list.conf" do
+  source "chroot_list.conf.erb"
   owner "root"
   group "root"
   mode 0644
-  action :create_if_missing
+  notifies :restart, "service[vsftpd]"
 end
